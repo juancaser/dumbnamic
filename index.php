8eb47a890e55c1b6e9f452dfef6a47b8f5d83a8e
The Juan Who Code <caserjan@gmail.com>
2013-10-18 00:26:35 +0800

    Add file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Dumb-Namic
+ *
+ * The not-so-dynamic-static-file driven web framework
+ * 
+ * @version b0.1
+ * @author The Juan Who Code <caserjan@gmail.com>
+ * @package index
+ */
+
+define('NICE_URL',true);
+define('DOMAIN','http://www.dumbnamic.dev');
+
+
+
+
+
+ 
+
+?>
